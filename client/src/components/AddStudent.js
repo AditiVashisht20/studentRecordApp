53a1b613cf37907227baeeaefc44c99105514351
unknown <aditivashisht20211@gmail.com>
2021-09-07 18:24:34 +0530

    Add Subject Form added

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -8,52 +8,116 @@ class AddStudent extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state={
+            fname:null,
+            mname:null,
+            lname:null,
+            dob:null,
+            email:null,
+            address:null,
+            
+        }
+        this.setFirstName=this.setFirstName.bind()
+        this.setMidName=this.setMidName.bind()
+        this.setLname=this.setLname.bind()
+        this.setage=this.setage.bind()
+        this.setDob=this.setDob.bind()
+        this.setemail=this.setemail.bind()
+        this.setadd=this.setadd.bind()
 
     }
-
+    setFirstName(fname)
+    {
+        this.setState({
+            fname:fname,
+        })
+    }
+    setMidName(mname)
+    {
+        this.setState({
+            mname:mname,
+        })
+    }
+    setLname(lname)
+    {
+        this.setState({
+            lname:lname
+        })
+    }
+    setage(age)
+    {
+        this.setState({
+            age:age
+        })
+    }
+    setDob(dob)
+    {
+        this.setState({
+            dob:dob
+        })
+    }
+    setemail(email)
+    {
+        this.setState({
+            email:email
+        })
+    }
+    setadd(address)
+    {
+        this.setState({
+            address:address
+        })
+    }
     render() {
         
-        return (<Container className="bg-primary"><Form>
+        return (<Container className="bg-info"><Form>
   <Row className="mb-3">
+    
     <Form.Group as={Col} controlId="name">
       <Form.Label>Name</Form.Label>
-      <Form.Control type="name" placeholder="Enter name" />
+      <Form.Control required type="name" placeholder="Enter name" 
+      onChange={(e) => this.setFirstName(e.target.value)}/>
     </Form.Group>
 
     <Form.Group as={Col} controlId="mname">
       <Form.Label>Mid Name</Form.Label>
-      <Form.Control type="mname" placeholder="Enter Your middle name" />
+      <Form.Control type="mname" placeholder="Enter Your middle name"
+      onChange={(e) => this.setMidName(e.target.value)} />
     </Form.Group>
-  </Row>
-
-  <Form.Group className="mb-3" controlId="lname">
+    <Form.Group as={Col} controlId="lname">
     <Form.Label>Last name</Form.Label>
-    <Form.Control type="name" placeholder="Enter your Last name" />
+    <Form.Control required type="name" placeholder="Enter your Last name" 
+    onChange={(e) => this.setLName(e.target.value)}/>
   </Form.Group>
-
-  <Form.Group className="mb-3" controlId="age">
+  </Row>
+<Row className="mb-3">
+  <Form.Group as={Col} className="mb-3" controlId="age">
     <Form.Label>Age</Form.Label>
-    <Form.Control type="age" placeholder="Enter Your Age" />
+    <Form.Control type="age" placeholder="Enter Your Age" 
+    onChange={(e) => this.setage(e.target.value)}/>
   </Form.Group>
+  <Form.Group as={Col}controlId="dob">
+    <Form.Label>DOB</Form.Label>
+    <Form.Control required type="dob" placeholder=" YYYY-MM-DD"
+    onChange={(e) => this.setDob(e.target.value)} />
+  </Form.Group>
+  </Row>
 
   <Row className="mb-3">
-  <Form.Group className="mb-3" controlId="dob">
-    <Form.Label>DOB</Form.Label>
-    <Form.Control type="dob" placeholder=" YYYY-MM-DD" />
-  </Form.Group>
-  
-
-
-    <Form.Group as={Col} controlId="email">
+ <Form.Group as={Col} controlId="email">
       <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="Enter Your email"/>
+      <Form.Control required type="email" placeholder="Enter Your email"
+      onChange={(e) => this.setemail(e.target.value)}/>
     </Form.Group>
   </Row>
-
- 
-
-
-  <Button variant="warning" type="submit">
+  <Row className="mb-3">
+ <Form.Group as={Col} controlId="Address">
+      <Form.Label>Address</Form.Label>
+      <Form.Control required type="add" placeholder="Enter Your Address"
+      onChange={(e) => this.setadd(e.target.value)}/>
+    </Form.Group>
+  </Row>
+ <Button variant="danger" type="submit">
     Add Student
   </Button>
 </Form></Container>)
--- client/src/components/AddSubject.js
+++ client/src/components/AddSubject.js
@@ -1,14 +1,55 @@
 import React from 'react';
+import {
+    Form, Row,Col,Button, Container     
+    } from 'react-bootstrap'
+    
 
 class AddSubject extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state={
+            subcode:null,
+            subname:null,
+
+        }
+    }
+    setcode(subcode)
+    {
+        this.setState({
+            subcode:subcode,
+        })
+    }
+    setsubname(subname)
+    {
+        this.setState({
+            subname:subname,
+        })
     }
 
     render() {
-        return <h1>AddSubject</h1>
+        return (<Container className="bg-info"><Form>
+        <Row className="mb-3">
+          
+          <Form.Group as={Col} controlId="code">
+            <Form.Label>Enter Subject statusCode</Form.Label>
+            <Form.Control required type="code" placeholder="Enter SubCode" 
+            onChange={(e) => this.setcode(e.target.value)}/>
+
+          </Form.Group>
+          <Form.Group as={Col} controlId="subname">
+            <Form.Label>Enter Subject Name</Form.Label>
+            <Form.Control required type="name" placeholder="Enter Subname" 
+            onChange={(e) => this.setsubname(e.target.value)}/>
+
+          </Form.Group>
+          </Row>
+          <Button variant="danger" type="submit">
+    Add Subject
+  </Button></Form></Container>
+          )
     }
+    
 }
 
 export default AddSubject;
